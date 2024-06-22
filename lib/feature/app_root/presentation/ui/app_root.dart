efc30a68d2cbbeb679324f93bc52d8f05291cad8
Zabalkanskiy <azabalkanskij@hotmail.com>
2024-06-22 17:28:44 +0300

    refactor refactor main.dart add multiblocprovider and debugBanner false to app_root.dart

--- lib/feature/app_root/presentation/ui/app_root.dart
+++ lib/feature/app_root/presentation/ui/app_root.dart
@@ -4,17 +4,25 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:search_git_hub/feature/auth_screen/presentation/bloc/auth_bloc.dart';
 import 'package:search_git_hub/feature/auth_screen/presentation/bloc/auth_event.dart';
 import 'package:search_git_hub/feature/auth_screen/presentation/ui/auth_screen.dart';
+import 'package:search_git_hub/feature/search_screen/data/search_sceen_repository_impl.dart';
+import 'package:search_git_hub/feature/search_screen/domain/get_user_data_use_case_impl.dart';
+import 'package:search_git_hub/feature/search_screen/presentation/bloc/search_bloc.dart';
 
 class AppRoot extends StatelessWidget{
   const AppRoot({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<AuthBloc>(create: (context) => AuthBloc()
-      ..add(CheckSignIn()),
+    return  MultiBlocProvider(
+        providers: [
+          BlocProvider<AuthBloc>(create: (context) => AuthBloc()
+      ..add(CheckSignIn())),
+          BlocProvider<SearchBloc>(create: (context) => SearchBloc(GetUserDataUseCaseImpl(repository: SearchScreenRepositoryImpl())))
+        ],
         child: ResponsiveSizer(builder: (context, orientation, screenType){
           return const MaterialApp(
             localizationsDelegates: [DefaultMaterialLocalizations.delegate],
+            debugShowCheckedModeBanner: false,
 
             home: AuthScreen(),
           );
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:search_git_hub/feature/app_root/presentation/ui/app_root.dart';
-import 'package:search_git_hub/feature/search_screen/presentation/ui/SearchScreen.dart';
-import 'package:search_git_hub/sign_in.dart';
 
-import 'home_app.dart';
 
-void main() async {
+
+
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(
-      const MaterialApp(
-            localizationsDelegates: [DefaultMaterialLocalizations.delegate],
-
-            home:  AppRoot()),
-          );
+  await dotenv.load(fileName: ".env");
+  runApp(AppRoot());
 
 }
