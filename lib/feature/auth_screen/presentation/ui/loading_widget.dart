93f2cbd758880eda7f96a919195dc23e33f92165
Zabalkanskiy <azabalkanskij@hotmail.com>
2024-06-22 17:53:01 +0300

    change color

--- lib/feature/auth_screen/presentation/ui/loading_widget.dart
+++ lib/feature/auth_screen/presentation/ui/loading_widget.dart
@@ -14,7 +14,7 @@ class Loading extends StatelessWidget {
       body: Center(
         child: LoadingAnimationWidget.fourRotatingDots(
           size: 6.h,
-          color: Colors.white54,
+          color: Colors.blueAccent,
         ),
       ),
     );
