9b0232ff10f480e3006c790a6601c5c37615a6c3
Zabalkanskiy <azabalkanskij@hotmail.com>
2024-06-22 17:31:44 +0300

    refactor api_service.dart

--- lib/core/api/api_service.dart
+++ lib/core/api/api_service.dart
@@ -1,42 +1,11 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:github_sign_in_plus/github_sign_in_plus.dart';
-import 'package:search_git_hub/secret_key.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
 
-  static Future<String> gitHubSignIn(BuildContext context) async {
-    final GitHubSignIn gitHubSignIn = GitHubSignIn(
-      clientId: GITHUB_CLIENT_ID,
-      clientSecret: GITHUB_CLIENT_SECRET,
 
-      redirectUrl: 'https://seach-git-hub-1218.firebaseapp.com/__/auth/handler',
-      title: 'GitHub Connection',
-      centerTitle: false,
-    );
-
-    var result = await gitHubSignIn.signIn(context);
-    switch (result.status) {
-      case GitHubSignInResultStatus.ok:
-        debugPrint("SUCCESS RESULT");
-        debugPrint(result.token ?? "TOKEN_NOT_FOUND"); //token
-
-        debugPrint("SUCCESS RESULT");
-        return result.token ?? "TOKEN_NOT_FOUND";
-
-
-
-
-      case GitHubSignInResultStatus.cancelled:
-      case GitHubSignInResultStatus.failed:
-        debugPrint(result.errorMessage);
-        debugPrint("Error RESULT");
-        return "TOKEN_NOT_FOUND";
-
-    }
-  }
 
   static Future<List<String>> fetchGitHubUserData(String accessToken) async {
     // Запрос к GitHub API для получения данных пользователя
